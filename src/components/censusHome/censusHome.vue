4311b7467f1bcaeb028eac5b381c6be96a3cd820
xilaiNo.78 <xilaiNo.78@XILAINO78-PC>
2019-08-01 17:09:42 +0800

    new

---
+++ src/components/censusHome/censusHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="census-main">
+    <img src="static\img\uncultivated.png" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.census-main {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
+
